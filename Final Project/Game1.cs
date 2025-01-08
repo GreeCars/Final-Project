7dce490932000663ec8766d98f01cf0d1caf8ac6
Carsen Greenwood <362570749@GoTVDSB.ca>
2025-01-08 09:38:33 -0500

    Working

--- Final Project/Game1.cs
+++ Final Project/Game1.cs
@@ -4,11 +4,31 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Final_Project
 {
+    enum Screen
+    {
+        Menu,
+        Game,
+        End
+    }
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Rectangle window;
+
+        Texture2D blackTargetTexture;
+
+        Texture2D greenTargetTexture;
+
+        Texture2D blueTargetTexture;
+
+        Texture2D redTargetTexture;
+
+        Texture2D crosshairTexture;
+
+        Texture2D menuTexture;
+        Texture2D backgroundTexture;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -28,6 +48,13 @@ namespace Final_Project
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            blackTargetTexture = Content.Load<Texture2D>("black-target");
+            greenTargetTexture = Content.Load<Texture2D>("green-target");
+            blueTargetTexture = Content.Load<Texture2D>("blue-target");
+            redTargetTexture = Content.Load<Texture2D>("red-target");
+            crosshairTexture = Content.Load<Texture2D>("crosshair");
+            menuTexture = Content.Load<Texture2D>("menuBackground");
+            backgroundTexture = Content.Load<Texture2D>("shooting-room");
         }
 
         protected override void Update(GameTime gameTime)
